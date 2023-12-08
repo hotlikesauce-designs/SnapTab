759b4e882b4745e6b87650058767d698edaa6e8a
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-12-08 19:39:42 +0000

    updated root / home folder to categories#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "users#index"
+  root to: "categories#index"
 
 
   # Routes for the Category resource:

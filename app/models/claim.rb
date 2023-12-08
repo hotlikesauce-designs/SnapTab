b21d467a1253c1747a6abe9f55c9dfeb58c00d92
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-12-08 15:40:55 +0000

    updated message

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,13 +1,3 @@
-# == Schema Information
-#
-# Table name: claims
-#
-#  id           :integer          not null, primary key
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  line_item_id :integer
-#  user_id      :integer
-#
 class Claim < ApplicationRecord
   # Direct associations
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
@@ -15,4 +5,5 @@ class Claim < ApplicationRecord
 
   # Indirect associations
   # Validations
+  validates :user_id, presence: true
 end

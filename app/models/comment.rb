a14ac88d44f03aaf4fe3a385381f456fda1cb935
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-12-08 15:48:51 +0000

    overwrite

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: comments
-#
-#  id           :integer          not null, primary key
-#  body         :text
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  commenter_id :integer
-#  receipt_id   :integer
-#
 class Comment < ApplicationRecord
   # Direct Associations
   belongs_to :commenter, required: true, class_name: "User", foreign_key: "commenter_id"
@@ -19,4 +8,5 @@ class Comment < ApplicationRecord
   validates :receipt_id, presence: true
   validates :commenter_id, presence: true
   validates :body, presence: true
+  
 end
